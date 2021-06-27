6f04fc6d5d188046168134494f39cfcca77d84de
ManojSuryawanshi <86551153+ManojSuryawanshi@users.noreply.github.com>
2021-06-27 17:25:53 +0530

    manoj2 rahul
    update new manoj

--- myfist/myfist/urls.py
+++ myfist/myfist/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from rahul import views
+from rahul import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
